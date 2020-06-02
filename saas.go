9a96bea3873aefdcfdea1ff14b20aa0331220fe2
evalphobia <evalphobia@gmail.com>
2020-06-02 14:43:00 +0900

    fix(dep):

--- saas.go
+++ saas.go
@@ -3,19 +3,20 @@ package main
 import (
 	_ "cloud.google.com/go"
 	_ "github.com/Azure/azure-sdk-for-go"
+	_ "github.com/Azure/go-autorest/autorest"
 	_ "github.com/aliyun/alibaba-cloud-sdk-go"
 	_ "github.com/aws/aws-sdk-go"
 	_ "github.com/digitalocean/godo"
 
 	_ "github.com/akamai/AkamaiOPEN-edgegrid-golang"
 	_ "github.com/cloudflare/cloudflare-go"
-	_ "github.com/fastly/go-fastly"
+	_ "github.com/fastly/go-fastly/fastly"
 
 	_ "github.com/Medium/medium-sdk-go"
 	_ "github.com/adlio/trello"
 	_ "github.com/andygrunwald/go-jira"
-	_ "github.com/dghubble/go-twitter"
-	_ "github.com/google/go-github"
+	_ "github.com/dghubble/go-twitter/twitter"
+	_ "github.com/google/go-github/github"
 	_ "github.com/huandu/facebook"
 	_ "github.com/jszwedko/go-circleci"
 	_ "github.com/slack-go/slack"
--- waf.go
+++ waf.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "github.com/NYTimes/gizmo"
-	_ "github.com/ant0ine/go-json-rest"
+	_ "github.com/ant0ine/go-json-rest/rest"
 	_ "github.com/astaxie/beego"
 	_ "github.com/caddyserver/caddy"
 	_ "github.com/gin-gonic/gin"
